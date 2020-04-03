79d465fd88500c07ebf9eb6bfb69be3d506e6d5c
CmdrNorthpaw <thetuxkid@gmail.com>
2020-04-03 20:10:12 +0100

    Added chat colours.

--- src/main/java/cmdrnorthpaw/minecam/commands/commandCamera.java
+++ src/main/java/cmdrnorthpaw/minecam/commands/commandCamera.java
@@ -1,10 +1,9 @@
 package cmdrnorthpaw.minecam.commands;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
-import org.bukkit.World;
-import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -19,7 +18,7 @@ public class commandCamera implements CommandExecutor {
         if (sender instanceof Player) {
             Player player = (Player) sender;
             if (!player.hasPermission("minecam.use")) {
-                player.sendMessage("You do not have permission to use this command");
+                player.sendMessage(ChatColor.DARK_RED + "You do not have permission to use this command");
                 return true;
 
             }
@@ -27,17 +26,20 @@ public class commandCamera implements CommandExecutor {
                 position = player.getLocation();
                 mode = player.getGameMode();
                 player.setGameMode(GameMode.SPECTATOR);
-                player.sendMessage("Camera mode activated.");
+                player.sendMessage(ChatColor.GREEN + "Camera mode activated.");
                 return true;
             }
             else {
                 player.setGameMode(mode);
-                player.sendMessage("Camera mode deactivated");
+                player.sendMessage(ChatColor.RED + "Camera mode deactivated");
                 player.teleport(position);
-                player.sendMessage("You were teleported back to your original location");
+                player.sendMessage(ChatColor.DARK_PURPLE + "You were teleported back to your original location");
                 return true;
             }
 
+        }
+        else {
+            sender.sendMessage("This command must be run as a player!");
         }
         return false;
     }
