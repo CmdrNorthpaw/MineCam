c96bec1091c1714a36c4144437bc8bb532f5a63d
CmdrNorthpaw <thetuxkid@gmail.com>
2020-04-03 15:22:31 +0100

    Finally finished /camera command!

--- src/main/java/cmdrnorthpaw/minecam/commands/commandCamera.java
+++ src/main/java/cmdrnorthpaw/minecam/commands/commandCamera.java
@@ -1,32 +1,40 @@
 package cmdrnorthpaw.minecam.commands;
 
+import com.sun.istack.internal.Nullable;
 import org.bukkit.Color;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class commandCamera implements CommandExecutor {
+    public static GameMode mode = GameMode.SURVIVAL;
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player) {
             Player player = (Player) sender;
+            Location position = player.getLocation();
             if (!player.hasPermission("minecam.use")) {
                 player.sendMessage("You do not have permission to use this command");
                 return true;
 
             }
             if (!(player.getGameMode() == GameMode.SPECTATOR)) {
-                Location position = player.getLocation();
-                GameMode mode = player.getGameMode();
+                position = player.getLocation();
+                mode = player.getGameMode();
                 player.setGameMode(GameMode.SPECTATOR);
                 player.sendMessage("Camera mode activated.");
                 return true;
             }
             else {
-                player.sendMessage("You can't run this command as a spectator!");
+                player.setGameMode(mode);
+                player.sendMessage("Camera mode deactivated");
+                player.teleport(position);
+                player.sendMessage("You were teleported back to your original location");
             }
 
         }
