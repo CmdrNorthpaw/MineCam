244fbe15b1ca95bae0e621b18f564ac7c4e365c4
CmdrNorthpaw <thetuxkid@gmail.com>
2020-04-03 19:30:28 +0100

    Fixed bug with teleportation system.

--- src/main/java/cmdrnorthpaw/minecam/commands/commandCamera.java
+++ src/main/java/cmdrnorthpaw/minecam/commands/commandCamera.java
@@ -1,7 +1,10 @@
 package cmdrnorthpaw.minecam.commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -9,12 +12,12 @@ import org.bukkit.entity.Player;
 
 public class commandCamera implements CommandExecutor {
     public static GameMode mode = GameMode.SURVIVAL;
+    public static Location position = new Location(Bukkit.getServer().getWorlds().get(0), 0.0, 0.00, 0.00);
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player) {
             Player player = (Player) sender;
-            Location position = player.getLocation();
             if (!player.hasPermission("minecam.use")) {
                 player.sendMessage("You do not have permission to use this command");
                 return true;
