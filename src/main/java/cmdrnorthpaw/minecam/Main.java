c4b381e715f19f5436fffc3f6578b3b674fc6832
CmdrNorthpaw <thetuxkid@gmail.com>
2020-05-20 11:30:14 +0100

    Added code to create config.yml and data.yml.

--- src/main/java/cmdrnorthpaw/minecam/Main.java
+++ src/main/java/cmdrnorthpaw/minecam/Main.java
@@ -3,15 +3,21 @@ package cmdrnorthpaw.minecam;
 import cmdrnorthpaw.minecam.commands.commandCamera;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.Configuration;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+
 public final class Main extends JavaPlugin {
+    public static FileConfiguration config;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         this.getCommand("camera").setExecutor(new commandCamera());
+        config = this.getConfig();
 
     }
 
@@ -19,5 +25,20 @@ public final class Main extends JavaPlugin {
     public void onDisable() {
         // Plugin shutdown logic
 
+    }
+
+    private void createYmls() {
+        if (!getDataFolder().exists()) {
+            getDataFolder().mkdirs();
+        }
+        File config = new File(getDataFolder(), "config.yml");
+        if (!config.exists()) {
+            getLogger().info("Creating new config.yml");
+            saveDefaultConfig();
+        }
+        File data = new File(getDataFolder(), "data.yml");
+        if (!data.exists()) {
+            getLogger().info("Creating new data.yml");
+        }
     }
 }
